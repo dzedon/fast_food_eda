bef89d3084b271f72cfa524349eb2098ef970ece
Daniel <daniel.salzedo@globant.com>
2023-01-25 14:50:26 -0500

    wip: checking offset

--- src/consumer_transaction.py
+++ src/consumer_transaction.py
@@ -14,11 +14,19 @@ ORDER_CONFIRM_KAFKA_TOPIC = "orders_confirmed"
 DLQ_TOPIC = "orders_details_dlq"
 GROUP_ID = "MY_GROUP"
 
+
+def test_callback():
+    logging.info("Commit completed.")
+    print("Here")
+
+
 consumer = KafkaConsumer(
     enable_auto_commit=False,
     group_id=GROUP_ID,
     bootstrap_servers="localhost:29092",
+    default_offset_commit_callback=test_callback(),
 )
+
 
 consumer.subscribe([ORDER_KAFKA_TOPIC])
 
@@ -27,16 +35,19 @@ producer = KafkaProducer(bootstrap_servers="localhost:29092")
 logging.info("Start listening...")
 
 for message in consumer:
+    topic_partition = TopicPartition(message.topic, message.partition)
+    offset = OffsetAndMetadata(message.offset, message.timestamp)
+    consumed_message = json.loads(message.value.decode())
+
     try:
         logging.info("Ongoing transaction..")
-        consumed_message = json.loads(message.value.decode())
 
         user_id = consumed_message.get("user_id")
         total_cost = consumed_message.get("total_cost")
         order_id = consumed_message.get('order_id')
 
         # TEMP
-        logging.info(f"offset: {message.offset}")
+        logging.info(f"***offset: {message.offset}")
 
         if int(order_id) % 2 == 0:
             raise Exception
@@ -50,18 +61,18 @@ for message in consumer:
 
         producer.send(ORDER_CONFIRM_KAFKA_TOPIC, json.dumps(data).encode("utf-8"))
 
-        topic_partition = TopicPartition(message.topic, message.partition)
-        offset = OffsetAndMetadata(message.offset + 1, message.timestamp)
-
         consumer.commit({topic_partition: offset})
         # consumer.commit()
-        logging.info("Success transaction..")
+        logging.info("******Success transaction..")
 
     except Exception:
         logging.exception("Couldn't process message.")
         consumed_message = json.loads(message.value.decode())
         # consumed_message['order_id'] += 1
         producer.send(DLQ_TOPIC, json.dumps(consumed_message).encode("utf-8"))
+
+    finally:
+        logging.info(f"**ultimo commit: {consumer.committed(partition=topic_partition)}")
 
 
 
